271e15afb5af54c849007a191517ea75c4ada9f0
Johannes <johannes.pfeffer@consensys.net>
2017-10-12 09:42:32 +0200

    move to turtle as ontology format

--- newSpecGenerator.py
+++ newSpecGenerator.py
@@ -23,10 +23,10 @@ class SpecGenerator():
 
 def main():
     text = "The EthOn Ethereum ontology, described using W3C RDF Schema and the Web Ontology Language. It is closely aligned with Gavin Wood's Ethereum yellow paper."
-    SpecGenerator(rdf_path="https://raw.githubusercontent.com/ConsenSys/EthOn/master/EthOn.rdf", output_path="spec", onto_title="EthOn: Ethereum Ontology", onto_text=text)
+    SpecGenerator(rdf_path="https://raw.githubusercontent.com/ConsenSys/EthOn/master/EthOn.ttl", output_path="spec", onto_title="EthOn: Ethereum Ontology", onto_text=text)
     text = "The ERC20 extension of the EthOn Ethereum ontology, described using W3C RDF Schema and the Web Ontology Language. It describes the token standard suggested by Fabian Vogelsteller in Ethereum Improvement Proposal (EIP) #20 and implemented in late 2015. For more information see https://github.com/ethereum/eips/issues/20"
-    SpecGenerator(rdf_path="https://raw.githubusercontent.com/ConsenSys/EthOn/master/ERC20/EthOn_ERC20.rdf", output_path="spec/ERC20", onto_title="EthOn: ERC20 Extension", onto_text=text)
+    SpecGenerator(rdf_path="https://raw.githubusercontent.com/ConsenSys/EthOn/master/ERC20/EthOn_ERC20.ttl", output_path="spec/ERC20", onto_title="EthOn: ERC20 Extension", onto_text=text)
     text = "The Contracts extension of the EthOn Ethereum ontology, described using W3C RDF Schema and the Web Ontology Language. It describes concepts and properties specific to smart contracts."
-    SpecGenerator(rdf_path="https://raw.githubusercontent.com/ConsenSys/EthOn/master/Contracts/EthOn_Contracts.rdf", output_path="spec/Contracts", onto_title="EthOn: Contracts Extension", onto_text=text)
+    SpecGenerator(rdf_path="https://raw.githubusercontent.com/ConsenSys/EthOn/master/Contracts/EthOn_Contracts.ttl", output_path="spec/Contracts", onto_title="EthOn: Contracts Extension", onto_text=text)
 
 main()
